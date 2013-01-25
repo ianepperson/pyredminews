4be9f63f68d8b48658b2513f0c311a808b48bbca
Ian Epperson <ian@epperson.com>
2013-01-25 00:11:22 -0800

    Finished bulk of base objects, added issues and projects
    rm = Redmine(url)
    rm.issues.get(21)
    rm.issues[21]
    rm.projects['test']

--- redmine/redmine.py
+++ redmine/redmine.py
@@ -19,74 +19,28 @@ import xml.etree.ElementTree as ET
 import json
 from types import MethodType
 
-
-# In order to handle the proper wrappers for unknown types, we need to correctly
-# work out the plural container info from the singular type
-#   project -> projects
-#   news -> news
-#   issue_category -> issue_categories
-#   etc.
-# Subset from http://code.activestate.com/recipes/577781-pluralize-word-convert-singular-word-to-its-plural/
-
-# Adding a plural to the map will correct unusual English plurals
-# and will also speed up the lookup.
-KNOWN_PLURAL_MAP = {
-	'':'',
-    'appendix': 'appendices',
-    'child': 'children',
-    'criterion': 'criteria',
-    'echo': 'echoes',
-    'index': 'indices',
-    'news': 'news',
-    'issue_status': 'issue_statuses',
-    'project': 'projects',
-    'issue': 'issues',
-    }
-
-VOWELS = set('aeiou')
-
-def pluralize(singular, lookup=KNOWN_PLURAL_MAP):
-	'''Return a plural word given the singular word'''
-	try:
-		return lookup[singular]
-	except:
-		pass
-	root = singular
-	try:
-		if singular[-1] == 'y' and singular[-2] not in VOWELS:
-			root = singular[:-1]
-			suffix = 'ies'
-		elif singular[-1] == 's':
-			if singular[-2] in VOWELS:
-				if singular[-3:] == 'ius':
-					root = singular[:-2]
-					suffix = 'i'
-				else:
-					root = singular[:-1]
-					suffix = 'ses'
-			else:
-				suffix = 'es'
-		elif singular[-2:] in ('ch', 'sh'):
-			suffix = 'es'
-		else:
-			suffix = 's'
-	except IndexError:
-		suffix = 's'
-	return root + suffix
+class RedmineError(StandardError):
+	pass
 
 
-class _Redmine_Item(object):
+class Redmine_Item(object):
 	'''Base class used for all items returned from Redmine.'''
 	# List out the attributes that aren't user-settable
 	_protected_attr = ['id']
 	_type = ''
-	_path = None
+	_changes = None
 	
-	def __init__(self, redmine, json_data=None, data={}):
+	def __init__(self, redmine, update_fn, json_data=None, data={}):
 		self._redmine = redmine
+		self._update_fn = update_fn
+		
 		if json_data:
 			# Parse the data
-			data = json.loads(json_data)
+			try:
+				data = json.loads(json_data)
+			except ValueError:
+				# If parsing failed, then raise the string which likely contains an error message instead of data
+				raise RedmineError(json_data)
 		if self._type:
 			try:
 				data = data[self._type]
@@ -94,17 +48,93 @@ class _Redmine_Item(object):
 				pass
 		# Map all dictionary data to object attributes
 		self.__dict__.update(data)
+
+		# Set the changes dict to track all changes from here on out
+		self._changes = {}
 		
 	def __setattr__(self, name, value):
 		'''Set the attribute for any non-protected attribute.'''
 		if name in self._protected_attr:
 			raise AttributeError("Can't set attribute %s." % name)
+		# Track any new changes for later saving
+		try:
+			self._changes[name] = value
+		except TypeError:
+			pass
+		
+		# Set the instance value
 		self.__dict__[name] = value
 
-	def save(self):
-		pass
+	def save(self, notes=None):
+		'''Save all changes (if any) back to Redmine.'''
+		if not self._changes:
+			return None
+		try:
+			self._update_fn(self.id, **self._changes)
+		except:
+			raise
+		else:
+			# Successful save, woot! Now clear the changes dict
+			self._changes.clear()
 
-class _Project(_Redmine_Item):
+
+class Redmine_Item_Control(object):
+	_object = Redmine_Item
+	
+	_query_path = ''
+	_query_container = ''
+	_item_path = ''
+	_item_new_path = ''
+	
+	def __init__(self, redmine):
+		self._redmine = redmine
+	
+	def __getitem__(self, key):
+		# returned when self[key] is called
+		return self.get(key)
+	
+	def _objectify(self, json_data):
+		'''Return an object derived from the given json data.'''
+		return self._object(self._redmine, self.update, json_data=json_data)
+	
+	def new(self, **dict):
+		'''Create a new item with the provided dict information.  Returns the new item.'''
+		if not self._item_new_path:
+			raise AttributeError('new is not available for %s' % self._type)
+		target = self._item_new_path
+		payload = json.dumps({self._object._type:dict})	
+		json_data = self._redmine.post(target, payload)
+		return self._objectify(json_data)
+	
+	def get(self, id):
+		'''Get a single item with the given ID'''
+		if not self._item_path:
+			raise AttributeError('get is not available for %s' % self._type)
+		target = self._item_path % id
+		json_data = self._redmine.get(target)
+		return self._objectify(json_data)
+	
+	def update(self, id, **dict):
+		'''Update a given item with the passed data.'''
+		if not self._item_path:
+			raise AttributeError('update is not available for %s' % self._type)
+		target = self._item_path % id
+		payload = json.dumps({self._object._type:dict})	
+		self._redmine.put(target, payload)
+		return None	
+		
+	def delete(self, id):
+		'''Delete a single item with the given ID'''
+		if not self._item_path:
+			raise AttributeError('delete is not available for %s' % self._type)
+		target = self._item_path % id
+		self._redmine.delete(target)
+		return None
+	
+		
+
+
+class Project(Redmine_Item):
 	'''Object representing a Redmine project.
 	Returned by Redmine getProject calls.
 	'''
@@ -117,40 +147,31 @@ class _Project(_Redmine_Item):
 					   'identifier',
 					   ]
 	
+	def __init__(self, redmine, *args, **kw_args):
+		# Override init to also set up sub-queries
+		# Call the base-class init
+		super(Project, self).__init__(redmine, *args, **kw_args)
+		
+		# Create our own issues controller,
+		# using __dict__ to bypass the changes__dict__
+		self.__dict__['issues'] = Issue_Control(redmine)
+		# Bake this project ID into queries and new issue commands
+		self.issues._query_path = '/projects/%s/issues.json' % self.id
+		self.issues._item_new_path = self.issues._query_path
+			
 	def __repr__(self):
 		return '<Redmine project #%s "%s">' % (self.id, self.identifier)
 	
-	def save(self):
-		'''Save changes for this project back to the server.'''
-		raise NotImplementedError('sorry')
+class Project_Control(Redmine_Item_Control):
+	_object = Project
+	_query_container = 'projects'
+	_query_path = '/projects.json'
+	_item_path = '/projects/%s.json'
+	_item_new_path = '/projects.json'
 	
-	def new_issue(self, **data):
-		'''Create a new issue for this project from the given pairs.
-		
-		newIssue( subject="The Arguments Department is closed", description="Good morning." )
-		
-		Possible keys are:
-		 subject
-		 description
-		 status_id
-		 priority_id
-		 tracker_id - can be looked up from name in Project.trackers[name]
-		 assigned_to_id
-		'''
-		if not 'subject' in data:
-			TypeError('Subject cannot be blank.  Use subject="This parrot is dead."')
-		if 'id' in data:
-			TypeError('Cannot set an ID.  You\'ll get that from the database.')
-		data[ 'project_id' ] = self.number
-		return self._redmine.newIssueFromDict( data )
-
-	def get_issues(self ):
-		'''Get the issue for this project.'''
-		raise NotImplementedError('sorry')
-		#todo: finish
 
 
-class _Issue(_Redmine_Item):
+class Issue(Redmine_Item):
 	'''Object representing a Redmine issue.'''
 	id = None
 	subject = None
@@ -167,38 +188,66 @@ class _Issue(_Redmine_Item):
 	def __str__(self):
 		return '<Redmine issue #%s, "%s">' % (self.id, self.subject)
 
-	def resolve(self):
-		'''Resolve this issue'''
-		self._redmine.resolve_issue( self.id )
+	def save(self, notes=None):
+		'''Save all changes back to Redmine with optional notes.'''
+		# Capture the notes if given
+		if notes:
+			self._changes['notes'] = notes
 		
-	def close(self):
-		'''Close this issue'''
-		self._redmine.close_issue( self.id )
+		# Check the changes dict for fields that need remapping
+		# Several fields need to go from field['id'] to field_id
+		for tag in ['assigned_to', 'project', 'category', 'status', 'parent_issue']:
+			try:
+				value = self._changes[tag]
+			except:
+				continue
+			
+			tag_id = tag + '_id'
+			try:
+				# Remap the ID change to the required field
+				self._changes[tag_id] = value['id']
+			except KeyError:
+				try:
+					# Try and grab the id of the object
+					self._changes[tag_id] = value.id
+				except AttributeError:
+					# If the changes field is not a dict or object, just use whatever value was given
+					self._changes[tag_id] = value
+			
+			# Remove the assigned_to from the changed data
+			del self._changes[tag]
+			
 		
-	def save(self):
-		'''Saves this issue - updates or creates new issue as necessary.  Failed updates DO NOT return any errors.'''
-		raise NotImplementedError('sorry')
-		#todo: finish
+		# Call the base-class save function
+		super(Issue, self).save()
 
-class _Redmine_CRUD(object):
-	def __init__(self, redmine, type_str, obj=_Redmine_Item):
-		self._redmine = redmine
-		self._type_str = type_str
-		self._obj = obj
+	def set_status(self, new_status, notes=None):
+		'''Save all changes and set to the given new_status'''
+		self.status_id = new_status
+		try:
+			self.status['id'] = self.status_id
+			# We don't have the id to name mapping, so blank the name
+			self.status['name'] = None
+		except:
+			pass
+		self.save(notes)		
+
+	def resolve(self, notes=None):
+		'''Save all changes and resolve this issue'''
+		self.set_status( self._redmine.ISSUE_STATUS_ID_RESOLVED, notes=notes )
+		
+	def close(self, notes=None):
+		'''Save all changes and close this issue'''
+		self.set_status( self._redmine.ISSUE_STATUS_ID_CLOSED, notes=notes )
+
+class Issue_Control(Redmine_Item_Control):
+	_object = Issue
+	_query_container = 'issues'
+	_query_path = '/issues.json'
+	_item_path = '/issues/%s.json'
+	_item_new_path = '/issues.json'
 	
-	def new(self, **kw_args):
-		data = self._redmine.new_item(self._type_str, **kw_args)
-		return self._obj(data)
-	def get(self, id=None):
-		if id:
-			data = self._redmine.get_item(self._type_str, id)
-		else:
-			raise NotImplementedError('sorry')
-		return self._obj(self._redmine, json_data=data)
-	def update(self, **kw_args):
-		pass
-	def delete(self, id):
-		pass
+
 
 class Redmine(object):
 	'''Class to interoperate with a Redmine installation using the REST web services.
@@ -217,15 +266,7 @@ class Redmine(object):
 	ISSUE_STATUS_ID_NEW = 1
 	ISSUE_STATUS_ID_RESOLVED = 3
 	ISSUE_STATUS_ID_CLOSED = 5		
-	
-	# Grab a global reference to the plural word map
-	KNOWN_PLURAL_MAP = KNOWN_PLURAL_MAP
-	
-	TYPE_OBJECT = {
-		'issue': _Issue,
-		'project': _Project,
-		}
-	
+		
 	def __init__(self, url, key=None, username=None, password=None, debug=False, readonlytest=False ):
 		self._url = url
 		self._key = key
@@ -238,9 +279,9 @@ class Redmine(object):
 		
 		self._opener = None
 		
-		if not username:
-			username = key
-			self._key = None
+		#if not username:
+		#	username = key
+		#	self._key = None
 			
 		if not password:
 			password = '12345'  #the same combination on my luggage!  (required dummy value)
@@ -265,7 +306,10 @@ class Redmine(object):
 		else:
 			if not key:
 				pass
-				
+			
+		# Create objects to manipulate different types
+		self.issues = Issue_Control(self)
+		self.projects = Project_Control(self)	
 		
 
 	# extend the request to handle PUT command
@@ -287,7 +331,7 @@ class Redmine(object):
 			
 		# if we're using a key, add it to the parms array
 		if self._key:
-			parms['key'] = self.__key
+			parms['key'] = self._key
 		
 		# encode any data
 		urldata = ''
@@ -352,88 +396,88 @@ class Redmine(object):
 			return self.open( page, HTTPrequest=self.DELETE_Request )
 
 
-			
-	def dict2XML(self, tag, dict ):
-		'''Return an XML string encoding the given dict'''
-		root = ET.Element( tag )
-		for key in dict:
-			ET.SubElement( root, str(key) ).text = str(dict[key])
-		
-		return ET.tostring( root, encoding='UTF-8' )
-		
-	
-	def new_item(self, type, **dict):
-		'''Create a new item'''
-		target = '%s.json' % type
-		payload = json.dumps(dict)
-		return self.post( target, payload)
-	
-	def get_item(self, type, id):
-		'''Get a given item by id'''
-		target = '%s/%s.json' % (type, id)
-		return self.get(target)
-	
-	def get_items(self, type):
-		'''Get a group of items'''
-		target = '%s.json' % type
-		return self.get(target)
-	
-	def update_item(self, type, id, **dict):
-		'''Update an item at the path with a given id'''
-		target = '%s/%s.json' % (type, id)
-		payload = json.dumps(dict)
-		return self.put(target, payload)
-	
-	def delete_item(self, type, id):
-		target = '%s/%s.json' % (type, id)
-		self.delete(target)
-
-	
-	@classmethod
-	def handle_type(cls, new_type):
-		'''Instruct this class to handle a new type of data.'''
-		if new_type == 'type':
-			raise RuntimeError('You can\'t set a type of type!')
-		for action, fn in cls.generic_action.items():
-			method_name = action + '_' + new_type
-			def fn_wrap(self, *kargs, **kw_args):
-				fn(self, new_type, *kargs, **kw_args)
-			cls.__dict__[action+'_'+new_type] = MethodType(fn_wrap, None, cls)
-			
-	def get_project(self, id ):
-		'''returns a project object for the given project name'''
-		#return self.Project( self.get('projects/'+projectIdent+'.xml') )
-		url = 'projects/%s.json' % id
-		return _Project( self, json_data=self.open(url), wrapper='project' )
-		
-	def get_issue(self, id ):
-		'''returns an issue object for the given issue number'''
-		#return self.Issue( self.get('issues/'+str(issueID)+'.xml') )
-		url = 'issues/%s.json' % id
-		return _Issue(self, json_data=self.open(url), wrapper='issue' )
-		
-	def newIssueFromDict(self, dict ):
-		'''creates a new issue using fields from the passed dictionary.  Returns the issue number or None if it failed. '''
-		xmlStr = self.dict2XML( 'issue', dict )
-		newIssue = self.Issue( self.post( 'issues.xml', xmlStr ) )
-		return newIssue
-	
-	def updateIssueFromDict(self, ID, dict ):
-		'''updates an issue with the given ID using fields from the passed dictionary'''
-		xmlStr = self.dict2XML( 'issue', dict )
-		self.put( 'issues/'+str(ID)+'.xml', xmlStr )
-
-	def delete_issue(self, ID ):
-		'''delete an issue with the given ID.  This can't be undone - use carefully!
-		Note that the proper method of finishing an issue is to update it to a closed state.'''
-		self.delete( 'issues/'+str(ID)+'.json' )
-		
-	def close_issue(self, ID ):
-		'''close an issue by setting the status to self.ISSUE_STATUS_ID_CLOSED'''
-		self.updateIssueFromDict( ID, {'status_id':self.ISSUE_STATUS_ID_CLOSED} )
-		
-	def resolve_issue(self, ID ):
-		'''close an issue by setting the status to self.ISSUE_STATUS_ID_RESOLVED'''
-		self.updateIssueFromDict( ID, {'status_id':self.ISSUE_STATUS_ID_RESOLVED} )
-		
-	
+#			
+#	def dict2XML(self, tag, dict ):
+#		'''Return an XML string encoding the given dict'''
+#		root = ET.Element( tag )
+#		for key in dict:
+#			ET.SubElement( root, str(key) ).text = str(dict[key])
+#		
+#		return ET.tostring( root, encoding='UTF-8' )
+#		
+#	
+#	def new_item(self, type, **dict):
+#		'''Create a new item'''
+#		target = '%s.json' % type
+#		payload = json.dumps(dict)
+#		return self.post( target, payload)
+#	
+#	def get_item(self, type, id):
+#		'''Get a given item by id'''
+#		target = '%s/%s.json' % (type, id)
+#		return self.get(target)
+#	
+#	def get_items(self, type):
+#		'''Get a group of items'''
+#		target = '%s.json' % type
+#		return self.get(target)
+#	
+#	def update_item(self, type, id, **dict):
+#		'''Update an item at the path with a given id'''
+#		target = '%s/%s.json' % (type, id)
+#		payload = json.dumps(dict)
+#		self.put(target, payload)
+#	
+#	def delete_item(self, type, id):
+#		target = '%s/%s.json' % (type, id)
+#		self.delete(target)
+#
+#	
+#	@classmethod
+#	def handle_type(cls, new_type):
+#		'''Instruct this class to handle a new type of data.'''
+#		if new_type == 'type':
+#			raise RuntimeError('You can\'t set a type of type!')
+#		for action, fn in cls.generic_action.items():
+#			method_name = action + '_' + new_type
+#			def fn_wrap(self, *kargs, **kw_args):
+#				fn(self, new_type, *kargs, **kw_args)
+#			cls.__dict__[action+'_'+new_type] = MethodType(fn_wrap, None, cls)
+#			
+#	def get_project(self, id ):
+#		'''returns a project object for the given project name'''
+#		#return self.Project( self.get('projects/'+projectIdent+'.xml') )
+#		url = 'projects/%s.json' % id
+#		return _Project( self, json_data=self.open(url), wrapper='project' )
+#		
+#	def get_issue(self, id ):
+#		'''returns an issue object for the given issue number'''
+#		#return self.Issue( self.get('issues/'+str(issueID)+'.xml') )
+#		url = 'issues/%s.json' % id
+#		return _Issue(self, json_data=self.open(url), wrapper='issue' )
+#		
+#	def newIssueFromDict(self, dict ):
+#		'''creates a new issue using fields from the passed dictionary.  Returns the issue number or None if it failed. '''
+#		xmlStr = self.dict2XML( 'issue', dict )
+#		newIssue = self.Issue( self.post( 'issues.xml', xmlStr ) )
+#		return newIssue
+#	
+#	def updateIssueFromDict(self, ID, dict ):
+#		'''updates an issue with the given ID using fields from the passed dictionary'''
+#		xmlStr = self.dict2XML( 'issue', dict )
+#		self.put( 'issues/'+str(ID)+'.xml', xmlStr )
+#
+#	def delete_issue(self, ID ):
+#		'''delete an issue with the given ID.  This can't be undone - use carefully!
+#		Note that the proper method of finishing an issue is to update it to a closed state.'''
+#		self.delete( 'issues/'+str(ID)+'.json' )
+#		
+#	def close_issue(self, ID ):
+#		'''close an issue by setting the status to self.ISSUE_STATUS_ID_CLOSED'''
+#		self.updateIssueFromDict( ID, {'status_id':self.ISSUE_STATUS_ID_CLOSED} )
+#		
+#	def resolve_issue(self, ID ):
+#		'''close an issue by setting the status to self.ISSUE_STATUS_ID_RESOLVED'''
+#		self.updateIssueFromDict( ID, {'status_id':self.ISSUE_STATUS_ID_RESOLVED} )
+#		
+#	
